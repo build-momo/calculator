5f6e8e84046c2d237a697452ddc2a7360255f161
build-momo <hongphuc.phamnguyen@ijgn.jp>
2020-05-20 11:30:12 +0900

    [add] new files

---
+++ script.js
@@ -0,0 +1,19 @@
+function BMI() {
+    var h=document.getElementById('h').value;
+    var w=document.getElementById('w').value;
+    var bmi=w/(h/100*h/100);
+    var bmio=(bmi.toFixed(1));
+
+    document.getElementById("result").innerHTML="Your BMI is " + bmio;
+
+    if (bmi < 18.5) {
+        return "Underweight";
+    } else if (bmi < 25) {
+        return "Normal";
+    } else if (bmi < 30) {
+        return "Overweight";
+    } else {
+        return "Obese";
+    }
+    
+    }
