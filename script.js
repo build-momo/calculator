70c04c74a39a6386ceffd0fe010a006ff9ebb68a
build-momo <hongphuc.phamnguyen@ijgn.jp>
2020-05-21 10:28:27 +0900

    [add] type text color

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 function BMI() {
     var h=document.getElementById('h').value;
     var w=document.getElementById('w').value;
-    var bmi=w/(h/100*h/100);
-    var bmio=(bmi.toFixed(1));
+ 
+    if(w > 0 && h > 0){	
+        var bmi = w/(h/100*h/100);
+        var bmio=(bmi.toFixed(1));
+		document.getElementById('result').innerHTML='Your BMI is ' + bmio;
+ 
+		if(bmi < 18.5){
+            document.getElementById('type').innerHTML = 'You are too thin.';
+            document.getElementById('type').style.color = '#e63946';
+		}
+		if(bmi >= 18.5 && bmi < 25){
+            document.getElementById('type').innerHTML = 'You are healthy.';
+            document.getElementById('type').style.color = '#2a9d8f';
+		}
+		if(bmi >= 25 && bmi < 30){
+            document.getElementById('type').innerHTML = 'You are overweight.';
+            document.getElementById('type').style.color = '#f4a261';
+        }
+        if(bmi >= 30){
+            document.getElementById('type').innerHTML = 'You are obese.';
+            document.getElementById('type').style.color = '#e63946';
+		}
+	}
+	else{
+		alert('Please enter your weight and height!')
+	}
 
-    document.getElementById("result").innerHTML="Your BMI is " + bmio;
-
-    if (bmi < 18.5) {
-        return "Underweight";
-    } else if (bmi < 25) {
-        return "Normal";
-    } else if (bmi < 30) {
-        return "Overweight";
-    } else {
-        return "Obese";
     }
     
-    }
